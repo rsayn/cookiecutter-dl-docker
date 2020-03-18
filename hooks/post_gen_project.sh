03d32b974875f34a8e4c373c65d2d4696a72ca92
Riccardo Sayn <riccardo.sayn@gmail.com>
2020-03-18 16:05:15 +0100

    Change post_gen hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,3 +2,7 @@
 
 # Add the ssh-credentials.conf file to .gitignore
 echo "ssh-credentials.conf" >> {{cookiecutter.project_slug}}/.gitignore
+
+# Make `/bin` and `/scripts` folder files executable
+chmod +x {{cookiecutter.project_slug}}/bin/*.sh
+chmod +x {{cookiecutter.project_slug}}/scripts/*.sh
